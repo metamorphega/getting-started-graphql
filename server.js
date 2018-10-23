75721c91f089b0e1c32020bf1736b71f1070595e
Your Name <vega@youexpress.co.id>
2018-10-23 18:09:48 +0700

    update basic type using rolldice

--- server.js
+++ server.js
@@ -3,18 +3,51 @@ let graphqlHTTP = require('express-graphql');
 // let { graphql, buildSchema } = require('graphql'); #0
 let { buildSchema } = require('graphql');
 
-let schema = buildSchema(`
-    type Query {
-        hello: String
-    }`);
+// #1
+// let schema = buildSchema(`
+//     type Query {
+//         hello: String
+//     }`);
 
-let root = {
-  hello: () => {
-    return 'Hello World!';
+// let root = {
+//   hello: () => {
+//     return 'Hello World!';
+//   },
+// };
+
+var schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// #1
+// let app = express();
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true,
+//   }),
+// );
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
   },
 };
 
-let app = express();
+var app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
@@ -23,9 +56,12 @@ app.use(
     graphiql: true,
   }),
 );
-
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+// #1
+// app.listen(4000);
+// console.log('Running a GraphQL API server at localhost:4000/graphql');
 
 // #0
 // graphql(schema, '{hello}', root).then(response => {
